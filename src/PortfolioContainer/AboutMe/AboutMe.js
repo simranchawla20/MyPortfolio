a8abc754f67d27a92bfa2d15fa222bdc07b6da7d
simranorigo20 <102580262+simranorigo20@users.noreply.github.com>
2022-07-13 15:44:55 +0530

    about me heading done

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -3,6 +3,24 @@ import './AboutMe.css'
 
 export default function AboutMe() {
   return (
-    <div className='about-container'>About me</div>
+    <div className='about-container'>
+      <div className='about-parent'>
+        <div className='heading-container'>
+          <div className='screen-heading'>
+            <span>About Me</span>
+          </div>
+          <div className='screen-sub-heading'>
+            <span>Why Choose Me?</span>
+          </div>
+          <div className='heading-separater'>
+            <div className='separater-line'></div>
+            <div className='separater-blob'>
+              <div></div>
+            </div>
+          </div>
+        </div>
+        <div className='about-card'></div>
+      </div>
+    </div>
   )
 }
